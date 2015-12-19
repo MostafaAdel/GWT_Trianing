118f6c0effb771a0ea43c739d7a7aea4ebe765d4
MostafaAdel <mostafa.adel90@gmail.com>
2015-12-20 00:14:17 +0400

    Initial Commit For GWT Course

---
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -0,0 +1,54 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Label;
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class StockWatcher implements EntryPoint {
+
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private FlexTable stocksFlexTable = new FlexTable();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private TextBox newSymbolTextBox = new TextBox();
+	private Button addStockButton = new Button("Add");
+	private Label lastUpdatedLabel = new Label();
+	
+	@Override
+	public void onModuleLoad() {
+		 // Create table for stock data.
+	    stocksFlexTable.setText(0, 0, "Symbol");
+	    stocksFlexTable.setText(0, 1, "Price");
+	    stocksFlexTable.setText(0, 2, "Change");
+	    stocksFlexTable.setText(0, 3, "Remove");
+	    
+	 // Assemble Add Stock panel.
+	    addPanel.add(newSymbolTextBox);
+	    addPanel.add(addStockButton);
+
+	    // Assemble Main panel.
+	    mainPanel.add(stocksFlexTable);
+	    mainPanel.add(addPanel);
+	    mainPanel.add(lastUpdatedLabel);
+
+	    // Associate the Main panel with the HTML host page.
+	    RootPanel.get("stockList").add(mainPanel);
+
+	    // Move cursor focus to the input box.
+	    newSymbolTextBox.setFocus(true);
+	    
+	    // TODO Assemble Add Stock panel.
+	    // TODO Assemble Main panel.
+	    // TODO Associate the Main panel with the HTML host page.
+	    // TODO Move cursor focus to the input box.
+		
+		
+	}
+	
+}
